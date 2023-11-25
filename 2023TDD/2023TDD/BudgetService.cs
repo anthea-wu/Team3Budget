bd96bd1d1df31d484a872ccb4696dce94392119a
shawn zhang <shawn.zhang@titansoft.com.sg>
2023-11-25 17:48:40 +0800

    init

---
+++ 2023TDD/2023TDD/BudgetService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NSubstitute;
+
+namespace _2023TDD;
+
+public class BudgetService
+{
+    private readonly IBudgetRepo _budgetRepo;
+
+    public BudgetService(IBudgetRepo budgetRepo)
+    {
+        _budgetRepo = budgetRepo;
+    }
+
+    public decimal Query(DateTime start, DateTime end)
+    {
+        if (start>end)
+        {
+            return 0;
+        }
+        var budgets = _budgetRepo.GetAll();
+
+        var queryTime = new DateTime(start.Year,start.Month,start.Day);
+
+        var totalBudget = 0;
+    
+        while(queryTime <= end || (queryTime.Month == end.Month))
+        {
+            if (start.Year == end.Year && start.Month == end.Month)
+            {
+                var queryDays = end.Day -start.Day+ 1;
+                totalBudget += GetDailyBudget(start, budgets) * queryDays;
+ 
+            }
+            else if (queryTime.Year == start.Year && queryTime.Month==start.Month)
+            {
+                var queryDays = DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1;
+
+                totalBudget += GetDailyBudget(start, budgets) * queryDays;
+            }
+            else if (queryTime.Year == end.Year && queryTime.Month == end.Month)
+            {
+                totalBudget +=  GetDailyBudget(end, budgets) * end.Day;
+            }
+            else
+            {
+                totalBudget +=  GetDailyBudget(queryTime, budgets) * DateTime.DaysInMonth(queryTime.Year,queryTime.Month);
+            }
+
+            queryTime = queryTime.AddMonths(1);
+        }
+        
+        return totalBudget;
+    }
+
+    private static int GetDailyBudget(DateTime budgetTime, List<Budget> budgets)
+    {
+        var budget = budgets.FirstOrDefault(x => x.YearMonth == $"{budgetTime.Year}{budgetTime.Month.ToString("00")}");
+        if (budget==null)
+        {
+            return 0;
+        }
+        var dailyBudget = budget.Amount / DateTime.DaysInMonth(budgetTime.Year, budgetTime.Month);
+        return dailyBudget;
+    }
+}
+
+public interface IBudgetRepo
+{
+    List<Budget> GetAll();
+}
+
+public class Budget
+{
+    public string YearMonth { get; set; }
+    public int Amount { get; set; }
+}
---
+++ 2023TDD/2023TDD/UnitTest1.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+
+namespace _2023TDD;
+
+public class BudgetServiceTests
+{
+    private IBudgetRepo _budgetRepo;
+    private BudgetService _budgetService;
+
+    [Test]
+    public void query_single_day()
+    {
+        GivenBudgetFromRepo(new List<Budget>()
+        {
+            new()
+            {
+                YearMonth = "202311",
+                Amount = 300
+            }
+        });
+
+        DateTime queryDate = new DateTime(2023, 11, 25);
+        Assert.AreEqual(10, _budgetService.Query(queryDate, queryDate));
+    }
+
+    [Test]
+    public void query_multi_days_in_same_month()
+    {
+        GivenBudgetFromRepo(new List<Budget>()
+        {
+            new()
+            {
+                YearMonth = "202311",
+                Amount = 300
+            }
+        });
+
+        DateTime start = new DateTime(2023, 11, 25);
+        DateTime end = new DateTime(2023, 11, 27);
+        Assert.AreEqual(30, _budgetService.Query(start, end));
+    }
+    
+    [Test]
+    public void query_multi_days_in_different_month()
+    {
+        GivenBudgetFromRepo(new List<Budget>()
+        {
+            new()
+            {
+                YearMonth = "202311",
+                Amount = 300
+            },new()
+            {
+                YearMonth = "202310",
+                Amount = 620
+            },
+        });
+
+        DateTime start = new DateTime(2023, 10, 30);
+        DateTime end = new DateTime(2023, 11, 3);
+        Assert.AreEqual(70, _budgetService.Query(start, end));
+    }
+    [Test]
+    public void query_multi_days_across_three_month()
+    {
+        GivenBudgetFromRepo(new List<Budget>()
+        {
+            new()
+            {
+                YearMonth = "202311",
+                Amount = 300
+            },new()
+            {
+                YearMonth = "202310",
+                Amount = 620
+            },new()
+            {
+                YearMonth = "202309",
+                Amount = 900
+            },
+        });
+
+        DateTime start = new DateTime(2023, 09, 30);
+        DateTime end = new DateTime(2023, 11, 3);
+        Assert.AreEqual(680, _budgetService.Query(start, end));
+    }
+   [Test]
+    public void query_multi_days_with_null_budget_setting()
+    {
+        GivenBudgetFromRepo(new List<Budget>()
+        {
+            new()
+            {
+                YearMonth = "202311",
+                Amount = 300
+            },new()
+            {
+                YearMonth = "202309",
+                Amount = 900
+            },
+        });
+
+        DateTime start = new DateTime(2023, 09, 30);
+        DateTime end = new DateTime(2023, 11, 3);
+        Assert.AreEqual(60, _budgetService.Query(start, end));
+    }
+    
+    [Test]
+    public void query_multi_days_across_year()
+    {
+        GivenBudgetFromRepo(new List<Budget>()
+        {
+            new()
+            {
+                YearMonth = "202312",
+                Amount = 310
+            },new()
+            {
+                YearMonth = "202401",
+                Amount = 620
+            }
+        });
+
+        DateTime start = new DateTime(2023, 12, 30);
+        DateTime end = new DateTime(2024, 01, 1);
+        Assert.AreEqual(40, _budgetService.Query(start, end));
+    }
+
+    [Test]
+    public void query_invalid_query_day()
+    {
+        
+
+        DateTime start = new DateTime(2023, 11, 4);
+        DateTime end = new DateTime(2023, 11, 3);
+        Assert.AreEqual(0, _budgetService.Query(start, end));
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _budgetRepo = Substitute.For<IBudgetRepo>();
+        _budgetService = new BudgetService(_budgetRepo);
+    }
+
+    private void GivenBudgetFromRepo(List<Budget> budgets)
+    {
+        _budgetRepo.GetAll().Returns(budgets);
+    }
+}
