6bc4faa99c895d64827b07ead337a311371fb295
Anthea Wu <anthea.wu2@titansoft.com.sg>
2023-11-26 14:13:56 +0800

    test: add failed test for no overlap period

--- 2023TDD/2023TDD/UnitTest1.cs
+++ 2023TDD/2023TDD/UnitTest1.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using FluentAssertions;
 using NSubstitute;
-using NSubstitute.Core;
 using NUnit.Framework;
 
 namespace _2023TDD;
@@ -16,7 +13,7 @@ public class BudgetServiceTests
     [Test]
     public void query_single_day()
     {
-        GivenBudgetFromRepo(new List<Budget>()
+        GivenBudgetFromRepo(new List<Budget>
         {
             new()
             {
@@ -25,14 +22,14 @@ public class BudgetServiceTests
             }
         });
 
-        DateTime queryDate = new DateTime(2023, 11, 25);
+        var queryDate = new DateTime(2023, 11, 25);
         Assert.AreEqual(10, _budgetService.Query(queryDate, queryDate));
     }
 
     [Test]
     public void query_multi_days_in_same_month()
     {
-        GivenBudgetFromRepo(new List<Budget>()
+        GivenBudgetFromRepo(new List<Budget>
         {
             new()
             {
@@ -41,104 +38,132 @@ public class BudgetServiceTests
             }
         });
 
-        DateTime start = new DateTime(2023, 11, 25);
-        DateTime end = new DateTime(2023, 11, 27);
+        var start = new DateTime(2023, 11, 25);
+        var end = new DateTime(2023, 11, 27);
         Assert.AreEqual(30, _budgetService.Query(start, end));
     }
-    
+
     [Test]
     public void query_multi_days_in_different_month()
     {
-        GivenBudgetFromRepo(new List<Budget>()
+        GivenBudgetFromRepo(new List<Budget>
         {
             new()
             {
                 YearMonth = "202311",
                 Amount = 300
-            },new()
+            },
+            new()
             {
                 YearMonth = "202310",
                 Amount = 620
-            },
+            }
         });
 
-        DateTime start = new DateTime(2023, 10, 30);
-        DateTime end = new DateTime(2023, 11, 3);
+        var start = new DateTime(2023, 10, 30);
+        var end = new DateTime(2023, 11, 3);
         Assert.AreEqual(70, _budgetService.Query(start, end));
     }
+
     [Test]
     public void query_multi_days_across_three_month()
     {
-        GivenBudgetFromRepo(new List<Budget>()
+        GivenBudgetFromRepo(new List<Budget>
         {
             new()
             {
                 YearMonth = "202311",
                 Amount = 300
-            },new()
+            },
+            new()
             {
                 YearMonth = "202310",
                 Amount = 620
-            },new()
+            },
+            new()
             {
                 YearMonth = "202309",
                 Amount = 900
-            },
+            }
         });
 
-        DateTime start = new DateTime(2023, 09, 30);
-        DateTime end = new DateTime(2023, 11, 3);
+        var start = new DateTime(2023, 09, 30);
+        var end = new DateTime(2023, 11, 3);
         Assert.AreEqual(680, _budgetService.Query(start, end));
     }
-   [Test]
+
+    [Test]
     public void query_multi_days_with_null_budget_setting()
     {
-        GivenBudgetFromRepo(new List<Budget>()
+        GivenBudgetFromRepo(new List<Budget>
         {
             new()
             {
                 YearMonth = "202311",
                 Amount = 300
-            },new()
+            },
+            new()
             {
                 YearMonth = "202309",
                 Amount = 900
-            },
+            }
         });
 
-        DateTime start = new DateTime(2023, 09, 30);
-        DateTime end = new DateTime(2023, 11, 3);
+        var start = new DateTime(2023, 09, 30);
+        var end = new DateTime(2023, 11, 3);
         Assert.AreEqual(60, _budgetService.Query(start, end));
     }
-    
+
     [Test]
     public void query_multi_days_across_year()
     {
-        GivenBudgetFromRepo(new List<Budget>()
+        GivenBudgetFromRepo(new List<Budget>
         {
             new()
             {
                 YearMonth = "202312",
                 Amount = 310
-            },new()
+            },
+            new()
             {
                 YearMonth = "202401",
                 Amount = 620
             }
         });
 
-        DateTime start = new DateTime(2023, 12, 30);
-        DateTime end = new DateTime(2024, 01, 1);
+        var start = new DateTime(2023, 12, 30);
+        var end = new DateTime(2024, 01, 1);
         Assert.AreEqual(40, _budgetService.Query(start, end));
     }
 
     [Test]
+    public void no_overlap()
+    {
+        GivenBudgetFromRepo(new List<Budget>
+        {
+            new()
+            {
+                YearMonth = "202312",
+                Amount = 310
+            },
+            new()
+            {
+                YearMonth = "202401",
+                Amount = 620
+            }
+        });
+
+        var start = new DateTime(2024, 3, 30);
+        var end = new DateTime(2024, 4, 1);
+        Assert.AreEqual(0, _budgetService.Query(start, end));
+    }
+
+
+    [Test]
     public void query_invalid_query_day()
     {
-        
-
-        DateTime start = new DateTime(2023, 11, 4);
-        DateTime end = new DateTime(2023, 11, 3);
+        var start = new DateTime(2023, 11, 4);
+        var end = new DateTime(2023, 11, 3);
         Assert.AreEqual(0, _budgetService.Query(start, end));
     }
 
